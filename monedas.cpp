4d80560ebd6a6bcf5c61898581cb544fd882fd7f
saraalonso3 <salonsop302@gmail.com>
2023-11-08 13:33:46 +0100

    He modificado los ficheros

--- monedas.cpp
+++ monedas.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 #include <limits>
 #include<algorithm>
+
+int *vectorSolucion (int *v, int **matrix, int N, int M) {
+        int *x = new int[N];
+        for(int i = 0; i < N; i++){
+                x[i] = 0;
+        }
+        int i = N-1; int j = M;
+
+        while(i > 0 && j > 0){
+                if( matrix[i][j] = 1 + matrix[i][j - v[i]] ){
+                        x[i] = x[i] + 1;
+                        j = j - v[i];
+                }
+                else{
+                        i = i - 1;
+                }
+        }
+
+        for(int i = 0; i < N; i++){
+                std::cout<<x[i]<<" ";
+        }
+
+	std::cout<<std::endl;
+        return x;
+}
+
+
+
 int cambio (int N, int *v, int M) {
 	int** matrix = new int*[N];
 	for(int i=0; i<N; i++){
@@ -22,63 +50,51 @@ int cambio (int N, int *v, int M) {
 			 if( j == 0){
 				matrix[i][j] = 0;
 			 }
-			 if ( i == 0 && v[i] > j){
-				int infinito = std::numeric_limits<int>::max();
-                                matrix[i][j] = infinito;
+			 else if ( i == 0 && v[i] > j){
+				//int infinito = std::numeric_limits<int>::max();
+                                matrix[i][j] = 0;
 			 }
-			 if ( i == 0){
-				matrix[i][j] = 1 + matrix[1, j - v[i]];
+			 else if ( i == 0){
+				matrix[i][j] = 1 + matrix[i][j - v[i]];
 			 }
-			 if( v[i] > j){
-				 matrix[i][j] = matrix[i -1, j];
+			 else if( v[i] > j){
+				 matrix[i][j] = matrix[i -1][j];
 			 }
 			 else{
-				 matrix[i][j] = std::min(matrix[i-1, j], 1 + matrix[i, j - v[i]] );
+				 matrix[i][j] = std::min(matrix[i-1][j], 1 + matrix[i][ j - v[i]] );
 			 }
 		 }
 	
-	}
+	} 
 
 	for(int i = 0; i < N; i++){
 		for(int j = 0;j <= M; j++){
+	
 			std::cout<<matrix[i][j]<< " ";
 		}
 		std::cout<<std::endl;
-	}
+		}
+	
+	int *x = vectorSolucion (v, matrix, N, M);
 
 	return matrix[N-1][M];
 }
 
-	
-
-
-
-int *vectorSolucion (int *v, int **matrix) {
-	int *x;
-	for(int i = 0; i < N; i++){
-		x[i] = 0;
-	}
-	int i = N-1; int j = M;
-	
-	while(i > 0 && j > 0){
-		if( matrix[i][j] = 1 + matrix[i][j - v[i]] ){
-			x[i] = x[i] + 1;
-			j = j - v[i];
-		}
-		else{
-			i = i - 1;
-		}
-	}
-	return x;
-}
 
 
 
 
 int main(){
-	int N = 3; int M = 8; int v[3]={1,4,6};
-	cambio(N, v, M);
-	vectorSolucion(v, **matrix);
+	int N = 3; int M = 8; int v[N];
+	v[0] = 1;
+	v[1] = 4;
+	v[2] = 6;
+
+	int minMonedas = cambio(N, v, M);
+	std::cout<< "Número mínimo de monedas necesarias: " << minMonedas << std::endl;
+
+  
+
 	return 0;
 }
 
